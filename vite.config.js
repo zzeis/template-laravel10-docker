4475cea6b00f2f967c247148fc30c39361516859
zzeis <moisesmonteiro.stark@gmail.com>
2025-04-15 14:04:26 -0300

    feat: vite is ok now

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
+import path from "path"; // Adicionando a importação do 'path'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+
+    server: {
+        host: "0.0.0.0", // Usa a variável de ambiente ou um valor padrão
+        port: 5173, // Porta configurável
+        strictPort: true,
+        cors: true,
+        hmr: {
+            host: "localhost", // Configurável via variável de ambiente
+            port: 5173,
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/js"),
+        },
+    },
 });
